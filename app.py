4efb3d7cca991d22ac89ba0ce197502669906dff
Ladonean <dominik01pitula@gmail.com>
2024-07-28 08:28:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ import streamlit as st
 import pandas as pd
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/jfif;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.header('`streamlit_pandas_profiling`')
-
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-st_profile_report(df)
+set_background('https://github.com/Ladonean/Nauka/blob/main/OIP.jfif')
