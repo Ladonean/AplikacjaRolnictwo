5274a7ae0fd31e98d1c54ef78d3ef52ab0990869
Ladonean <dominik01pitula@gmail.com>
2024-07-28 09:01:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-def load_css(file_name):
-    with open (file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+image='https://github.com/Ladonean/Nauka/blob/main/nature-fcp.png'
 
-load_css('style.css')
-        
-st.header('st.write')
-
-# Example 1
-
-st.write('Hello, *World!* :sunglasses:')
-
-# Example 2
-
-st.write(1234)
-
-# Example 3
-
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
-
+st.image(image, caption='natura')
 
