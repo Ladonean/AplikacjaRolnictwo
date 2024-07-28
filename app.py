697f786869c6fe47021b858eba465b001f140191
Ladonean <dominik01pitula@gmail.com>
2024-07-28 09:03:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-image='https://github.com/Ladonean/Nauka/blob/main/nature-fcp.png'
+image='https://github.com/Ladonean/Nauka/blob/main/R.jpg'
 
 st.image(image, caption='natura')
 
