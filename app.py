59a2944eff17bc0b45ce0c83965779c25e7349ab
Ladonean <dominik01pitula@gmail.com>
2024-07-28 08:39:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,24 @@ page_element="""
 }
 </style>
 """
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
 st.markdown(page_element, unsafe_allow_html=True)
 
