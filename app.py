0733ce4e09cd9ade88fb5908959f0479bed36dca
Ladonean <dominik01pitula@gmail.com>
2024-07-28 16:52:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,23 +14,6 @@ from io import StringIO
 
 
 
-
-
-
-        
-def wczytaj_stacje(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        st.error("Nie udało się pobrać danych stacji z podanego URL: " + url)
-        return None
-    data = response.content.decode('windows-1250')
-    df = pd.read_csv(StringIO(data), delimiter=',', header=None)
-    
-    df.columns = ['X', 'Y', 'Stacja']
-    df['X'] = df['X'].astype(float)
-    df['Y'] = df['Y'].astype(float)
-    
-    return df
     
 
 
