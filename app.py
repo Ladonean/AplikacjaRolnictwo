7ecd8723713e88da5863bf1ea224325c1da3d862
Ladonean <dominik01pitula@gmail.com>
2024-07-28 08:11:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,4 @@ from streamlit_pandas_profiling import st_profile_report
 st.header('`streamlit_pandas_profiling`')
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-
-pr = df.profile_report()
-st_profile_report(pr)
+st_profile_report(df)
