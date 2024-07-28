427b3e7fd63042f1b5ff01151186284b1f328eb9
Ladonean <dominik01pitula@gmail.com>
2024-07-28 16:49:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,47 +21,9 @@ st.set_page_config(
     }
 )
 
-# Funkcja do załadowania obrazu tła
-page_bg_img= """
-        <style>
-        /* Smooth scrolling*/
-        .main {
-        scroll-behavior: smooth;
-        }
-        /* main app body with less padding*/
-        .st-emotion-cache-z5fcl4 {
-        padding-block: 0;
-        }
 
 
-        .stApp {
-        background-color: #e6ffe6;
-        }
-        </style>
-        """
 
-# Dodaj tło z lokalnego pliku
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
-
-def date_input_proc(input_date, time_range):
-    end_date = input_date
-    start_date = input_date - timedelta(days=time_range)
-    
-    str_start_date = start_date.strftime('%Y-%m-%d')
-    str_end_date = end_date.strftime('%Y-%m-%d')
-    return str_start_date, str_end_date
-    
-def geocode_address(address):
-    geolocator = Photon(user_agent="measurements")
-    try:
-        location = geolocator.geocode(address)
-        if location:
-            return [location.latitude, location.longitude]
-        else:
-            return None
-    except GeocoderTimedOut:
-        return None
         
 def wczytaj_stacje(url):
     response = requests.get(url)
@@ -77,46 +39,21 @@ def wczytaj_stacje(url):
     
     return df
     
-path_stacje1 = 'https://raw.githubusercontent.com/Ladonean/FigDetect/main/Stacje.csv'
-st.write("lol")
-df = wczytaj_stacje(path_stacje1)
-st.dataframe(df)
+
 
 def main():
-    with st.sidebar:
-        st.title("App")
-        st.subheader("Menu:")
-        st.markdown(
-            """
-                - [Data](#data)
-                - [Mapa](#mapa)
-                - [Opady](#Opady)
-                - [About](#about)
-            """)
-        
-    with st.container():
-        st.title("App opady")
-        st.markdown("Aplikacja sluzaca do sprawdzania opadow i wskaznika ndvi na obszarze Polski")
 
-        
-    with st.container():
-
-                # Creating a 2 days delay for the date_input placeholder to be sure there are satellite images in the dataset on app start
-                today = datetime.today()
-                delay = today - timedelta(days=5)
-
-                # Date input widgets
-                st.success("Data P 📅")
-                initial_date = st.date_input("initial", value=delay, label_visibility="collapsed")
-    
+    path_stacje1 = 'https://raw.githubusercontent.com/Ladonean/FigDetect/main/Stacje.csv'
+    st.write("lol")
+    df = wczytaj_stacje(path_stacje1)
+    st.dataframe(df)
     with st.container():
         address = st.text_input("Wpisz adres:", "Czaple, Kartuzy")
         coords = geocode_address(address)
         if coords:
-            m = folium.Map(location = coords, zoom_start = 10, tiles="Esri.WorldImagery")
+            m = folium.Map(location = [53,18], zoom_start = 10, tiles="Esri.WorldImagery")
             folium.Marker(
-                location=coords,
-                popup=address,
+                location=[53,18]
             ).add_to(m)
 
             
@@ -127,8 +64,7 @@ def main():
             folium.LayerControl().add_to(m)
             
             st_folium (m, width=1600)
-        else:
-            st.write("Wrong")
+
 
 # Run the app
 if __name__ == "__main__":
