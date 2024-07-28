9f14fb551a22e4aa6d241c719a001c9ee855d271
Ladonean <dominik01pitula@gmail.com>
2024-07-28 20:21:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -98,8 +98,8 @@ def main():
             st.write("Nie udało się zlokalizować adresu.")
 
     with st.container():
-        csv_url1 = "https://raw.githubusercontent.com/Ladonean/Nauka/main/Stacje.csv?raw=true"
-        data = load_data(csv_url1)
+        csv_url = "https://raw.githubusercontent.com/Ladonean/Nauka/main/Stacje.csv?raw=true"
+        data = load_data(csv_url)
         st.dataframe(data)
 
 # Uruchomienie aplikacji
