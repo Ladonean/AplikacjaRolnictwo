cbf76685370daa84f91b6cd985954c06f3b0a555
Ladonean <dominik01pitula@gmail.com>
2024-07-28 08:36:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-import base64
 
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg('https://github.com/Ladonean/Nauka/blob/main/nature-fcp.png')
-
+page_element="""
+<style>
+[data-testid="stAppViewContainer"]{
+  background-image: url("https://github.com/Ladonean/Nauka/blob/main/nature-fcp.png");
+  background-size: cover;
+}
+[data-testid="stHeader"]{
+  background-color: rgba(0,0,0,0);
+}
+</style>
+"""
+st.markdown(page_element, unsafe_allow_html=True)
 
