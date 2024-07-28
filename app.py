c5b947f91097b5b21486e83362b6f3cd0b45b2b5
Ladonean <dominik01pitula@gmail.com>
2024-07-28 10:17:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    page_title="App",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+    'Repo': "https://github.com/Ladonean/Nauka/tree/main"
+    }
+)
+
 # Funkcja do załadowania obrazu tła
 page_bg_img= """
         <style>
+        /* Smooth scrolling*/
+        .main {
+        scroll-behavior: smooth;
+        }
+        /* main app body with less padding*/
+        .st-emotion-cache-z5fcl4 {
+        padding-block: 0;
+        }
         .stApp {
-                background-color: #e6ffe6;
-                }
+        background-color: #e6ffe6;
+        }
         </style>
         """
 
