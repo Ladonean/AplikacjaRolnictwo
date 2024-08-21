c02a532a9668a5132db0829ccfae1aaf49897a42
Ladonean <dominik01pitula@gmail.com>
2024-08-21 14:25:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -225,58 +225,58 @@ def main():
         coords = geocode_address(address)
 
 
-        # if coords:
+        if coords:
 
 
-        # # Ustawienia początkowe dat
-        #     ndvi_image, image_date = calculate_ndvi(start_date, end_date,coords)
-        #     ndwi_image = calculate_ndwi(start_date, end_date,coords)
+        # Ustawienia początkowe dat
+            ndvi_image, image_date = calculate_ndvi(start_date, end_date,coords)
+            ndwi_image = calculate_ndwi(start_date, end_date,coords)
 
-        #     ndvi_map_id_dict = geemap.ee_tile_layer(
-        #         ndvi_image,
-        #         vis_params={
-        #         'min': -0.3, 
-        #         'max': 0.8,
-        #         'palette': ['#a50026', '#d73027', '#fdae61', '#1a9850', '#006837']
-        #         },
-        #         name="NDVI"
-        #         )
+            ndvi_map_id_dict = geemap.ee_tile_layer(
+                ndvi_image,
+                vis_params={
+                'min': -0.3, 
+                'max': 0.8,
+                'palette': ['#a50026', '#d73027', '#fdae61', '#1a9850', '#006837']
+                },
+                name="NDVI"
+                )
             
             
-        #     ndwi_map_id_dict = geemap.ee_tile_layer(
-        #         ndwi_image,
-        #         vis_params={
-        #         'min': -0.6, 
-        #         'max': 0.3, 
-        #         'palette': ['#a50026', '#ffffbf', '#87a6ff']
-        #         },
-        #         name="NDWI"
-        #         )
+            ndwi_map_id_dict = geemap.ee_tile_layer(
+                ndwi_image,
+                vis_params={
+                'min': -0.6, 
+                'max': 0.3, 
+                'palette': ['#a50026', '#ffffbf', '#87a6ff']
+                },
+                name="NDWI"
+                )
             
-        #     m = folium.Map(location=coords, zoom_start=10, tiles="Esri.WorldImagery")
-        #     folium.Marker(
-        #         location=coords,
-        #         popup=address,
-        #     ).add_to(m)
+            m = folium.Map(location=coords, zoom_start=10, tiles="Esri.WorldImagery")
+            folium.Marker(
+                location=coords,
+                popup=address,
+            ).add_to(m)
 
-        #     m.add_child(ndvi_map_id_dict)
-        #     m.add_child(ndwi_map_id_dict)
+            m.add_child(ndvi_map_id_dict)
+            m.add_child(ndwi_map_id_dict)
 
 
-        #     folium.LayerControl().add_to(m)
+            folium.LayerControl().add_to(m)
 
-        #     st_folium(m, width=600, height=600)  # Dostosowanie szerokości i wysokości mapy
+            st_folium(m, width=600, height=600)  # Dostosowanie szerokości i wysokości mapy
         
-        # else:
-        #     st.write("Nie udało się zlokalizować adresu.")
+        else:
+            st.write("Nie udało się zlokalizować adresu.")
     
-    # with st.container():
-    #     st.success(f"Data zdjęcia satelity {image_date}")
+    with st.container():
+        st.success(f"Data zdjęcia satelity {image_date}")
     
-    # if st.button("Generuj Mapę"):
-    #     folium.LayerControl().add_to(m)
+    if st.button("Generuj Mapę"):
+        folium.LayerControl().add_to(m)
 
-    #     m.save("example_123.html")
+        m.save("example_123.html")
 
     with st.container():
         st.markdown('<h2 id="opady">Opady</h2>', unsafe_allow_html=True)
