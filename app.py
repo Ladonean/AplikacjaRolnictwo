d8d7efc43dbde7d3272011c8ea9b2daaa7e00167
Ladonean <dominik01pitula@gmail.com>
2024-08-30 19:49:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -80,6 +80,27 @@ def geocode_address(address):
 #ee.Authenticate() 
 #ee.Initialize(project='ee-ladone')
 
+def get_image(start_date, end_date, coords):
+    # Pobieranie kolekcji obrazów Landsat 8 (Collection 2 Level 2)
+    point = ee.Geometry.Point([coords[1], coords[0]])
+    buffer = point.buffer(10000)
+    collection = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED") \
+        .filterDate(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')) \
+        .filterBounds(buffer)
+    
+    # Zamiast median(), wybierz pierwszy obraz w kolekcji
+    image = collection.first()
+    
+    # Pobranie daty obrazu
+    image_date = image.get('system:time_start').getInfo()
+    
+    if image_date is not None:
+        image_date = datetime.fromtimestamp(image_date / 1000, tz=timezone.utc).strftime('%Y-%m-%d')
+    else:
+        image_date = "Brak dostępnej daty"
+    
+    return image, image_date, buffer
+
 
 
 # Wczytanie csv ze opadami
@@ -152,8 +173,10 @@ def plot_wynik(path_shp, Wynik, title):
 
     return fig, ax
 
+# Główna funkcja uruchamiająca aplikację
+def main():
 
-with st.sidebar:
+    with st.sidebar:
         st.title("Aplikacja Opady")
         st.subheader("Menu:")
         st.markdown(
@@ -164,143 +187,122 @@ with st.sidebar:
                 - [About](#about)
             """)
 
-with st.container():
-    st.title("Aplikacja Opady")
-    st.markdown("Aplikacja służąca do sprawdzania opadów i wskaźnika NDVI na obszarze Polski")
+    with st.container():
+        st.title("Aplikacja Opady")
+        st.markdown("Aplikacja służąca do sprawdzania opadów i wskaźnika NDVI na obszarze Polski")
 
-with st.container():
-    end_date = st.date_input("Wybierz datę końcową", value=datetime.today())
+    with st.container():
+        end_date = st.date_input("Wybierz datę końcową", value=datetime.today())
 
-    start_date = end_date - timedelta(weeks=2)
+        start_date = end_date - timedelta(weeks=2)
 
-    # st.success(f"Dzisiejsza data: {datetime.today()}")
-    # st.write(f"Wybrana data początkowa: {start_date.strftime('%Y-%m-%d')}")
-    # st.write(f"Wybrana data końcowa: {end_date.strftime('%Y-%m-%d')}")
-
-
-with st.container():
-    st.markdown('<h2 id="mapa">Mapa</h2>', unsafe_allow_html=True)
-    address = st.text_input("Wpisz adres:", "Czaple, Kartuzy")
-    coords = geocode_address(address)
+        # st.success(f"Dzisiejsza data: {datetime.today()}")
+        # st.write(f"Wybrana data początkowa: {start_date.strftime('%Y-%m-%d')}")
+        # st.write(f"Wybrana data końcowa: {end_date.strftime('%Y-%m-%d')}")
 
 
-    if coords:
+    with st.container():
+        st.markdown('<h2 id="mapa">Mapa</h2>', unsafe_allow_html=True)
+        address = st.text_input("Wpisz adres:", "Czaple, Kartuzy")
+        coords = geocode_address(address)
 
-        def get_image(start_date, end_date, coords):
-            # Pobieranie kolekcji obrazów Landsat 8 (Collection 2 Level 2)
-            point = ee.Geometry.Point([coords[1], coords[0]])
-            buffer = point.buffer(10000)
-            collection = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED") \
-                .filterDate(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')) \
-                .filterBounds(buffer)
+
+        if coords:
+
+
+        # Ustawienia początkowe dat
+            image, image_date, buffer = get_image(start_date, end_date,coords)
+
+
+            # Obliczanie NDVI dla wybranego obrazu
+            ndvi_image = image.normalizedDifference(['B5', 'B4']).rename('NDVI').clip(buffer)
+            # Obliczanie NDVI dla wybranego obrazu
+            ndwi_image = image.normalizedDifference(['B3', 'B5']).rename('NDVI').clip(buffer)
             
-            # Zamiast median(), wybierz pierwszy obraz w kolekcji
-            image = collection.first()
+            ndvi_map_id_dict = geemap.ee_tile_layer(
+                ndvi_image,
+                vis_params={
+                'min': -0.3, 
+                'max': 0.8,
+                'palette': ['#a50026', '#d73027', '#fdae61', '#1a9850', '#006837']
+                },
+                name="NDVI"
+                )
             
-            # Pobranie daty obrazu
-            image_date = image.get('system:time_start').getInfo()
             
-            if image_date is not None:
-                image_date = datetime.fromtimestamp(image_date / 1000, tz=timezone.utc).strftime('%Y-%m-%d')
-            else:
-                image_date = "Brak dostępnej daty"
+            ndwi_map_id_dict = geemap.ee_tile_layer(
+                ndwi_image,
+                vis_params={
+                'min': -0.6, 
+                'max': 0.3, 
+                'palette': ['#a50026', '#ffffbf', '#87a6ff']
+                },
+                name="NDWI"
+                )
             
-            return image, image_date, buffer
+            m = folium.Map(location=coords, zoom_start=10, tiles="Esri.WorldImagery")
+            folium.Marker(
+                location=coords,
+                popup=address,
+            ).add_to(m)
+
+            m.add_child(ndvi_map_id_dict)
+            m.add_child(ndwi_map_id_dict)
 
 
+            folium.LayerControl().add_to(m)
 
-        image, image_date, buffer = get_image(start_date, end_date,coords)
-
-
-        # Obliczanie NDVI dla wybranego obrazu
-        ndvi_image = image.normalizedDifference(['B5', 'B4']).rename('NDVI').clip(buffer)
-        # Obliczanie NDVI dla wybranego obrazu
-        ndwi_image = image.normalizedDifference(['B3', 'B5']).rename('NDVI').clip(buffer)
+            st_folium(m, width=600, height=600)  # Dostosowanie szerokości i wysokości mapy
         
-        ndvi_map_id_dict = geemap.ee_tile_layer(
-            ndvi_image,
-            vis_params={
-            'min': -0.3, 
-            'max': 0.8,
-            'palette': ['#a50026', '#d73027', '#fdae61', '#1a9850', '#006837']
-            },
-            name="NDVI"
-            )
-        
-        
-        ndwi_map_id_dict = geemap.ee_tile_layer(
-            ndwi_image,
-            vis_params={
-            'min': -0.6, 
-            'max': 0.3, 
-            'palette': ['#a50026', '#ffffbf', '#87a6ff']
-            },
-            name="NDWI"
-            )
-        
-        m = folium.Map(location=coords, zoom_start=10, tiles="Esri.WorldImagery")
-        folium.Marker(
-            location=coords,
-            popup=address,
-        ).add_to(m)
-
-        m.add_child(ndvi_map_id_dict)
-        m.add_child(ndwi_map_id_dict)
-
-
-        folium.LayerControl().add_to(m)
-
-        st_folium(m, width=600, height=600)  # Dostosowanie szerokości i wysokości mapy
-
-    else:
-        st.write("Nie udało się zlokalizować adresu.")
-
-with st.container():
-    st.success(f"Data zdjęcia satelity {image_date}")
-
-with st.container():
-    st.subheader("NDVI - Normalized Difference Vegetation Index")
-    st.write("""
-            Wskaźnik NDVI jest narzędziem niezwykle ważnym dla rolników, pozwalającym na monitorowanie plonów oraz wykrywanie problemów związanych z niedoborem wody czy chorobami roślin. 
-            Dzięki danym pochodzącym z satelitarnej obserwacji Ziemi, rolnicy mogą uzyskać szybki i dokładny obraz stanu roślinności na swoich uprawach.
-            """)
+        else:
+            st.write("Nie udało się zlokalizować adresu.")
     
-    st.subheader("NDWI - Normalized Difference Water Index")
-    st.write("""
-            Wskaźnik NDWI służy do monitorowania zmian związanych z zawartością wody w zbiornikach wodnych. 
-            Ponieważ zbiorniki wodne silnie absorbują światło w widzialnym i podczerwonym spektrum elektromagnetycznym, NDWI wykorzystuje pasma zielone i bliskiej podczerwieni, aby wyodrębnić zbiorniki wodne. 
-            Wskaźnik ten jest czuły na tereny zurbanizowane, co może prowadzić do przeszacowania obszarów wodnych.
-            """)
+    with st.container():
+        st.success(f"Data zdjęcia satelity {image_date}")
 
-# if st.button("Generuj Mapę"):
-#     folium.LayerControl().add_to(m)
-
-#     m.save("example_123.html")
-
-with st.container():
-    st.markdown('<h2 id="opady">Opady</h2>', unsafe_allow_html=True)
-    stacje_url = "https://raw.githubusercontent.com/Ladonean/Nauka/main/Stacje.csv?raw=true"
-    location_data = wczytaj_stacje(stacje_url)
-
-
-    opady_url = f'https://raw.githubusercontent.com/Ladonean/FigDetect/main/o_d_{end_date.strftime("%m")}_{end_date.strftime("%Y")}.csv'
-    rain_data = wczytaj_csv(opady_url)
-    if rain_data is not None:
-
-        merged_data = merge_data(location_data, rain_data, end_date)
-
-        st.dataframe(merged_data)
-        max_value = merged_data['Opady'].astype(float).max()
-        min_value = merged_data['Opady'].astype(float).min()
+    with st.container():
+        st.subheader("NDVI - Normalized Difference Vegetation Index")
+        st.write("""
+                Wskaźnik NDVI jest narzędziem niezwykle ważnym dla rolników, pozwalającym na monitorowanie plonów oraz wykrywanie problemów związanych z niedoborem wody czy chorobami roślin. 
+                Dzięki danym pochodzącym z satelitarnej obserwacji Ziemi, rolnicy mogą uzyskać szybki i dokładny obraz stanu roślinności na swoich uprawach.
+                """)
         
-        st.write(f"Maksymalna ilość opadów: {max_value}")
-        st.write(f"Minimalna ilość opadów: {min_value}")
+        st.subheader("NDWI - Normalized Difference Water Index")
+        st.write("""
+                Wskaźnik NDWI służy do monitorowania zmian związanych z zawartością wody w zbiornikach wodnych. 
+                Ponieważ zbiorniki wodne silnie absorbują światło w widzialnym i podczerwonym spektrum elektromagnetycznym, NDWI wykorzystuje pasma zielone i bliskiej podczerwieni, aby wyodrębnić zbiorniki wodne. 
+                Wskaźnik ten jest czuły na tereny zurbanizowane, co może prowadzić do przeszacowania obszarów wodnych.
+                """)
+    
+    # if st.button("Generuj Mapę"):
+    #     folium.LayerControl().add_to(m)
 
-        path_shp = 'https://raw.githubusercontent.com/Ladonean/FigDetect/main/gadm41_POL_1.shp'
-        # Rysowanie mapy
-        fig, ax = plot_wynik(path_shp, merged_data, f'Opady {end_date.strftime("%d")}-{end_date.strftime("%m")}-{end_date.strftime("%Y")}')
-        st.markdown('<h2 id="polska">Polska</h2>', unsafe_allow_html=True)
-        st.pyplot(fig)
+    #     m.save("example_123.html")
+
+    with st.container():
+        st.markdown('<h2 id="opady">Opady</h2>', unsafe_allow_html=True)
+        stacje_url = "https://raw.githubusercontent.com/Ladonean/Nauka/main/Stacje.csv?raw=true"
+        location_data = wczytaj_stacje(stacje_url)
+
+
+        opady_url = f'https://raw.githubusercontent.com/Ladonean/FigDetect/main/o_d_{end_date.strftime("%m")}_{end_date.strftime("%Y")}.csv'
+        rain_data = wczytaj_csv(opady_url)
+        if rain_data is not None:
+
+            merged_data = merge_data(location_data, rain_data, end_date)
+
+            st.dataframe(merged_data)
+            max_value = merged_data['Opady'].astype(float).max()
+            min_value = merged_data['Opady'].astype(float).min()
+            
+            st.write(f"Maksymalna ilość opadów: {max_value}")
+            st.write(f"Minimalna ilość opadów: {min_value}")
+
+            path_shp = 'https://raw.githubusercontent.com/Ladonean/FigDetect/main/gadm41_POL_1.shp'
+            # Rysowanie mapy
+            fig, ax = plot_wynik(path_shp, merged_data, f'Opady {end_date.strftime("%d")}-{end_date.strftime("%m")}-{end_date.strftime("%Y")}')
+            st.markdown('<h2 id="polska">Polska</h2>', unsafe_allow_html=True)
+            st.pyplot(fig)
 
     # with st.container():
     #     csv_url = "https://raw.githubusercontent.com/Ladonean/Nauka/main/Stacje.csv?raw=true"
@@ -309,4 +311,6 @@ with st.container():
 
 
 
-
+# Uruchomienie aplikacji
+if __name__ == "__main__":
+    main()
