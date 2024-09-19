5114c9e5d626b7752fb660681deedd1b360f4f93
Ladonean <dominik01pitula@gmail.com>
2024-09-19 11:19:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -258,7 +258,7 @@ def main():
 
         # Wyświetlanie mapy ze stanu sesji
         if 'map' in st.session_state:
-            st_folium(st.session_state['map'], width=1200, height=800)
+            st_folium(st.session_state['map'], width=1600, height=1000)
         else:
             st.write("Nie udało się zlokalizować adresu.")
 
